c7ee50a26597d8170cb6cb8eda597fd834e4c9da
way-indumentaria <way-indumentaria@outlook.com>
2022-11-15 23:55:53 -0300

    Modificaciones de  credenciales de MYSQL

--- src/routes/database.ts
+++ src/routes/database.ts
@@ -1,15 +1,31 @@
 //Importamos el metodo "createPool" que nos permitira conectarnos a la base de datos desde "promise-mysql"
 import { createPool } from "promise-mysql";
 
+import {
+    DB_HOST,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USER,
+    DB_PORT
+  } from "../config";
+
 //Funcion que se encargara de la conexion a las base de datos
 export async function conexion() 
 {
     //Constante en la que definiremos las propiedades de "createPool" en la constante "connect", para que poder conectarse a la base de datos
-    const connect = await createPool({
+    /*const connect = await createPool({
         host:'us-cdbr-east-04.cleardb.com',
         user:'b41346736cbe93',
         password:'5ebb0c4b',
         database:'heroku_184c871a0f3b411'
+    });*/
+
+    const connect = await createPool({
+        host:DB_HOST,
+        user:DB_USER,
+        password:DB_NAME,
+        port:DB_PORT,
+        database:DB_PASSWORD
     });
 
     //Entrega una respuesta
